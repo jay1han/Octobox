20b5893921c3d34069124e11dcc9bb6b2c6cf9a4
Jay Han <jayhan.name@gmail.com>
2025-04-25 11:54:40 +0200

    done

--- Server/install.sh
+++ Server/install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
-apt install python3-smbus
+# apt install python3-smbus
 
 mkdir -v /usr/share/octobox
-cp -v octo*.py /usr/share/octobox/
-cp -v ustreamer/ustreamer.bin /usr/share/octobox/ustreamer
+cp -pv octo*.py /usr/share/octobox/
+cp -pv ustreamer/ustreamer.bin /usr/share/octobox/ustreamer
 touch /usr/share/octobox/socket
 
 cp -v files/index.html /var/www/html
 cp -v files/localIP /var/www/html
 
-cp -v octocgi.py /var/www/bin/
+cp -pv octo_cgi.py /var/www/bin/
 cp -v files/octobox.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable octobox
