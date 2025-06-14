9e34cf923496dcf0af18c7082b5b5df1fc061ad8
Jay Han <jayhan.name@gmail.com>
2025-06-14 09:06:32 +0200

    status file

--- Server/octo_disp.py
+++ Server/octo_disp.py
@@ -17,11 +17,26 @@ def replaceText(filename, text):
 
 class Display:
     def __init__(self):
+        self._state = ''
+        self._filename = ''
+        self._completion = ''
+        self._remaining = ''
+        self._bedtemp = ''
+        
         self.jobInfo = JobInfo()
         self.started = None
         self.ended = None
         self.clearInfo()
         self.setState('Printer')
+
+    def writeStatus(self):
+        with open('/var/www/html/status.1', 'wt') as status:
+            print(f'state={self._state}', file=status)
+            print(f'filename={self._filename}', file=status)
+            print(f'completion={self._completion}', file=status)
+            print(f'remaining={self._remaining}', file=status)
+            print(f'bedtemp={self._bedtemp}', file=status)
+        os.rename('/var/www/html/status.1', '/var/www/html/status')
 
     def setupIP(self):
         list_if = subprocess.run(['/usr/bin/ip', 'addr'], capture_output=True, text=True).stdout
@@ -33,7 +48,9 @@ class Display:
         replaceText('/var/www/html/localIP', my_ip)
     
     def setState(self, statusText):
+        self._state = statusText
         replaceText('/var/www/html/state', statusText)
+        self.writeStatus()
 
     def setTemps(self, tempAmb, tempExt, tempBed, tempOut, tempCpu, tempCold):
         temps = '<tr>'
@@ -41,8 +58,10 @@ class Display:
         if tempExt != 0.0:
             temps += f'<td>Extruder : {tempExt:.1f}&deg;</td>'
         if tempBed != 0.0:
+            self._bedtemp = f'{tempBed:.1f}'
             temps += f'<td>Bed : {tempBed:.1f}&deg;</td>'
         elif tempOut != 0.0:
+            self._bedtemp = f'{tempOut:.1f}'
             temps += f'<td>Out : {tempOut:.1f}&deg;'
             if tempCold != 0.0:
                 temps += f' ({tempCold:.1f})'
@@ -52,6 +71,7 @@ class Display:
             temps += f'<td>Ambient: {tempAmb:.1f}&deg;</td>'
         temps += '</tr>'
         replaceText('/var/www/html/temps', temps)
+        self.writeStatus()
 
     def setElapsed(self, actualTime):
         jobInfoText = f'<tr><td>File</td><td colspan=3>{self.jobInfo.filename}</td></tr>'
@@ -59,6 +79,7 @@ class Display:
         jobInfoText += f'<td>Estimated</td><td>{printTime(self.jobInfo.fileTime)}</td></tr>'
         jobInfoText += f'<tr><td>Ended</td><td>{self.ended.strftime("%H:%M")}</td></tr>'
         replaceText('/var/www/html/jobInfo', jobInfoText)
+        self.writeStatus()
         
     def setJobInfo(self, jobInfo:JobInfo = None):
         if jobInfo is None:
@@ -69,19 +90,18 @@ class Display:
         jobInfoText = ''
         
         if jobInfo.filename != '':
+            self._filename = jobInfo.filename
             self.lastNow = datetime.now().strftime('%H:%M')
 
             if self.started is None:
                 if jobInfo.fileTime == 0:
                     jobInfoText = f'<tr><td>File</td><td colspan=3>{jobInfo.filename}</td></tr>'
-                    self.setQuery(jobInfo.filename, None, None)
                 else:
                     eta = (datetime.now() + timedelta(seconds = jobInfo.fileTime)).strftime('%H:%M')
                     jobInfoText = f'<tr><td>File</td><td colspan=3>{jobInfo.filename}</td></tr>'
                     jobInfoText += f'<tr><td>Estimated</td><td>{printTime(jobInfo.fileTime)}</td></tr>'
                     jobInfoText += f'<tr><td>Now</td><td>{datetime.now().strftime("%H:%M")}</td>'
                     jobInfoText += f'<td>ETA</td><td>{eta}</td></tr>'
-                    self.setQuery(jobInfo.filename, None, jobInfo.fileTime)
                        
             else:
                 remainingTime = 0
@@ -92,11 +112,15 @@ class Display:
                     remainingTime = jobInfo.fileTime - jobInfo.currentTime
                     eta = (self.started + timedelta(seconds = jobInfo.fileTime)).strftime('%H:%M')
                     donePercent = 100.0 * jobInfo.currentTime / jobInfo.fileTime
-                    
+                    self._completion = f'{donePercent:.0f}'
+                    self._remaining = f'{printTime(remainingTime)}'
+                
                 if self.ended is not None:
                     donePercent = 100.0
                     remainingTime = 0
-                    
+                    self._completion = ''
+                    self._remaining = ''
+
                 jobInfoText = f'<tr><td>File</td><td colspan=5>{jobInfo.filename}</td></tr>'
                 jobInfoText += f'<tr><td>Elapsed</td><td>{printTime(jobInfo.currentTime)} ({donePercent:.1f}%)</td>'
                 jobInfoText += f'<td>Remaining</td><td>{printTime(remainingTime)}</td>'
@@ -104,25 +128,16 @@ class Display:
                 jobInfoText += f'<tr><td>Started</td><td>{self.started.strftime("%H:%M")}</td>'
                 jobInfoText += f'<td>Now</td><td>{self.lastNow}</td>'
                 jobInfoText += f'<td>ETA</td><td>{eta}</td></tr>'
-                self.setQuery(jobInfo.filename, donePercent, remainingTime)
             
         replaceText('/var/www/html/jobInfo', jobInfoText)
-
-    def setQuery(self,
-                 filename: str = None,
-                 completion: float = None,
-                 remaining: int = None):
-        queryInfo = \
-            (f'filename={filename}\n' if filename is not None else '') + \
-            (f'completion={completion:.1}\n' if completion is not None else '') + \
-            (f'remaining={printTime(remaining)}\n' if remaining is not None else '')
-        replaceText('/var/www/html/query', queryInfo)
+        self.writeStatus()
 
     def clearInfo(self):
         self.setTemps(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
         self.setJobInfo(JobInfo())
         self.started = None
         self.ended = None
+        self.writeStatus()
 
     def start(self):
         self.started = datetime.now()
@@ -130,4 +145,3 @@ class Display:
     def end(self):
         self.ended = datetime.now()
         self.setJobInfo()
-        
